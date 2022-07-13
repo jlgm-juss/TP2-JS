b47d03bc8833925bb06cc165edac25199a8c309e
Jose Luis <juss.jlgm@gmail.com>
2022-07-13 19:14:01 -0300

    agregue tp2 punto 12

---
+++ JS/TP2punto12.js
@@ -0,0 +1,4 @@
+//FUNCIÓN Math.floor: redondea al número entero menor.
+    //FUNCIÓN Math.random: devuelve un número aleatorio mayor que 0 y menor que 1
+    let num = Math.floor((Math.random() * 99) + 1);
+    document.write(num);
